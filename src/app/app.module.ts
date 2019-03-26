95074438f9d1a2bed711be4b74b2010e6ade6bfc
Fernando Seguim <fernando.seguim@gmail.com>
2019-03-25 23:58:51 -0300

    create pets page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { PetsPageComponent } from './pets-page/pets-page.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
     NavbarComponent,
     LoginPageComponent,
     ResetPasswordPageComponent,
-    SignupPageComponent
+    SignupPageComponent,
+    PetsPageComponent
   ],
   imports: [
     BrowserModule,
