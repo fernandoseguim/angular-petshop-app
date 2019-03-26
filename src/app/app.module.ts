2c7b2f50546bf3848cc7514f3412db7ce0e1b918
Fernando Seguim <fernando.seguim@gmail.com>
2019-03-26 00:20:24 -0300

    create products-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { PetsPageComponent } from './pets-page/pets-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PetsPageComponent } from './pets-page/pets-page.component';
     LoginPageComponent,
     ResetPasswordPageComponent,
     SignupPageComponent,
-    PetsPageComponent
+    PetsPageComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
