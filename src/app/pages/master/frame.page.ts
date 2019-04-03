6c03d4fbb5c3a719285278b1daea66d37ee00f4a
Fernando Seguim <fernando.seguim@gmail.com>
2019-04-03 16:45:36 -0300

    create master page and routing

---
+++ src/app/pages/master/frame.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frame-component',
+  template: '<app-navbar></app-navbar><router-outlet></router-outlet>'
+})
+export class FramePageComponent { }
